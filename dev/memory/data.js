d32b80b8c2d79b99ae29dcf3cef47d8f5e788eaa
github-action-benchmark <github@users.noreply.github.com>
2026-07-11 17:26:23 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 8b0c9be846a2f7bc1151b5d2cd1621a5a35bed16

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783788856829,
+  "lastUpdate": 1783790783829,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -13254,6 +13254,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_hello_world",
             "value": 40.9,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 63.6,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "8b0c9be846a2f7bc1151b5d2cd1621a5a35bed16",
+          "message": "Assignments written inside a condition are now tracked",
+          "timestamp": "2026-07-11T19:14:04+02:00",
+          "tree_id": "c71d49f1021ddc29ee719d8ae5a6a6fee7263c98",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/8b0c9be846a2f7bc1151b5d2cd1621a5a35bed16"
+        },
+        "date": 1783790782494,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 40.8,
             "unit": "MiB"
           },
           {
