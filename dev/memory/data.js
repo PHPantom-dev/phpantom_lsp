c3986726be64ce2014a99339cf7ef1e56bcbb90c
github-action-benchmark <github@users.noreply.github.com>
2026-07-17 18:54:10 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for f240bfc4730a53edd4f97efbd5cb9a15b126ee20

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784313271737,
+  "lastUpdate": 1784314450957,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -16149,6 +16149,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 70.7,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "f240bfc4730a53edd4f97efbd5cb9a15b126ee20",
+          "message": "A closure parameter's declared type is kept when the collection's\nelement type is a partial union",
+          "timestamp": "2026-07-17T20:39:46+02:00",
+          "tree_id": "ab3f0ed9cb1813b009cccd639972870c5fdb5067",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/f240bfc4730a53edd4f97efbd5cb9a15b126ee20"
+        },
+        "date": 1784314448723,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 42.8,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 71.7,
             "unit": "MiB"
           }
         ]
