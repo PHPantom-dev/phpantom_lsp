21c642c437e3256308e38a8c073e7991006f8b8b
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2026-07-23 23:09:42 +0600

    chore(definition): handle CommandOwnParam in type-definition match

--- src/definition/type_definition.rs
+++ src/definition/type_definition.rs
@@ -151,6 +151,7 @@ impl Backend {
             | SymbolKind::NamespaceDeclaration { .. }
             | SymbolKind::LaravelStringKey { .. }
             | SymbolKind::LaravelMacroString { .. }
+            | SymbolKind::CommandOwnParam { .. }
             | SymbolKind::Keyword
             | SymbolKind::CastType
             | SymbolKind::Comment => {
