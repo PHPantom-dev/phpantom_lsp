4844292e82d1eaf933eac9f43ea33a1f6afbae05
github-action-benchmark <github@users.noreply.github.com>
2026-09-08 20:26:14 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 7ecf35e1ca386e54c5088092717daab582f060b6

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788898575322,
+  "lastUpdate": 1788899174728,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -27029,6 +27029,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 70.2,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "7ecf35e1ca386e54c5088092717daab582f060b6",
+          "message": "A directive between an echo's opening tag and its terminator no longer\nbreaks the rest of the template",
+          "timestamp": "2026-09-08T22:09:00+02:00",
+          "tree_id": "d60abdef3919d20b6ece5ca5374d94ed596867e3",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/7ecf35e1ca386e54c5088092717daab582f060b6"
+        },
+        "date": 1788899167634,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 37.3,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 78.4,
             "unit": "MiB"
           }
         ]
