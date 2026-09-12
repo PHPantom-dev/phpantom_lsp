566dc209c01e8221117376674b7b8ac024ea41f4
github-action-benchmark <github@users.noreply.github.com>
2026-09-12 01:40:58 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 76718932da40a7c945e78830559c34468509a943

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789173779037,
+  "lastUpdate": 1789177258207,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -27675,6 +27675,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 75.9,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "76718932da40a7c945e78830559c34468509a943",
+          "message": "Fix parsing unbalanced tags",
+          "timestamp": "2026-09-12T03:24:44+02:00",
+          "tree_id": "d4c5ece27652709d32209e9ea0e6175669a4a0ea",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/76718932da40a7c945e78830559c34468509a943"
+        },
+        "date": 1789177251548,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 37,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 73.1,
             "unit": "MiB"
           }
         ]
