d330c5204fd398feb21539c9d1841604ab43233c
github-action-benchmark <github@users.noreply.github.com>
2026-09-26 13:44:51 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 7fe1ce20e90daee6b6ba20a4174f94fd2c7de1ef

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790429200209,
+  "lastUpdate": 1790430291191,
   "repoUrl": "https://github.com/PHPantom-dev/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -32129,6 +32129,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 81.4,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "7fe1ce20e90daee6b6ba20a4174f94fd2c7de1ef",
+          "message": "A closure invalidates receiver state whether it is called inline or\nthrough a variable",
+          "timestamp": "2026-09-26T15:28:43+02:00",
+          "tree_id": "9590ed8782ba132ab3ffe1fe8e63f03b17807879",
+          "url": "https://github.com/PHPantom-dev/phpantom_lsp/commit/7fe1ce20e90daee6b6ba20a4174f94fd2c7de1ef"
+        },
+        "date": 1790430284671,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 37.9,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 74.4,
             "unit": "MiB"
           }
         ]
